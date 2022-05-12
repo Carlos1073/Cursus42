6ee80fe2a2bfb36e15a1f08c69aa66cab1a115ce
Carlos Guerrero garcia <caguerre@cbr9s6.42barcelona.com>
2022-05-12 11:43:45 +0200

    1

---
+++ Libft/ft_memchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caguerre <caguerre@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/11 10:56:12 by caguerre          #+#    #+#             */
+/*   Updated: 2022/05/11 10:58:47 by caguerre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*str;
+	unsigned char	character;
+
+	i = 0;
+	str = (unsigned char *)s;
+	character = (unsigned char)c;
+	while (i < n)
+	{
+		if (str[i] == character)
+			return (&str[i]);
+		i++;
+	}
+	return (NULL);
+}
