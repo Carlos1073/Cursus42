4e630d4964da0492e83c5ba592e523f2246eeefc
Carlos Guerrero garcia <caguerre@cbr9s6.42barcelona.com>
2022-05-12 11:48:50 +0200

    1

---
+++ Libft/ft_strnstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caguerre <caguerre@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/12 11:47:00 by caguerre          #+#    #+#             */
+/*   Updated: 2022/05/12 11:47:56 by caguerre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (needle[j] == '\0' || needle == NULL)
+		return ((char *)haystack);
+	i = 0;
+	while (haystack[i] != '\0' && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && i + j < len)
+		{
+			if (needle[j] == '\0')
+				return ((char *)&haystack);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
