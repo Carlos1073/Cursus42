efcfbe4e2f9e64cd06d40f88552459c6c345ee40
Carlos Guerrero garcia <caguerre@cbr7s4.42barcelona.com>
2022-05-15 10:48:30 +0200

    OK

--- Libft/ft_isascii.c
+++ Libft/ft_isascii.c
@@ -6,15 +6,15 @@
 /*   By: caguerre <caguerre@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 12:24:15 by caguerre          #+#    #+#             */
-/*   Updated: 2022/05/09 12:24:18 by caguerre         ###   ########.fr       */
+/*   Updated: 2022/05/12 16:03:48 by caguerre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 int	ft_isascii(int c)
 {
-	if (c >= 0 && c <= 177)
+	if (c >= 0 && c <= 127)
 		return (1);
 	else
 		return (0);
