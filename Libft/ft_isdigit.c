3c3bb083bec16a0da7be84c5b487f4dc7aea147b
Carlos Guerrero garcia <caguerre@cbr7s4.42barcelona.com>
2022-05-15 10:48:52 +0200

    OK

--- Libft/ft_isdigit.c
+++ Libft/ft_isdigit.c
@@ -6,11 +6,11 @@
 /*   By: caguerre <caguerre@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 12:04:44 by caguerre          #+#    #+#             */
-/*   Updated: 2022/05/09 12:07:07 by caguerre         ###   ########.fr       */
+/*   Updated: 2022/05/12 14:42:11 by caguerre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 int	ft_isdigit(int c)
 {
