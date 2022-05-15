c9936bbd63c3f39e51a187a2f5d1b7ec3e232b8d
Carlos Guerrero garcia <caguerre@cbr7s4.42barcelona.com>
2022-05-15 10:45:57 +0200

    OK

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,11 +6,11 @@
 /*   By: caguerre <caguerre@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 15:16:02 by caguerre          #+#    #+#             */
-/*   Updated: 2022/05/11 15:16:04 by caguerre         ###   ########.fr       */
+/*   Updated: 2022/05/12 16:54:10 by caguerre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 void	*ft_calloc(size_t count, size_t size)
 {
