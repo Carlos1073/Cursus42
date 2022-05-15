43491ad467de566c35983b79a1c91f397674c824
Carlos Guerrero garcia <caguerre@cbr7s4.42barcelona.com>
2022-05-15 10:47:57 +0200

    OK

--- Libft/ft_isalnum.c
+++ Libft/ft_isalnum.c
@@ -6,11 +6,11 @@
 /*   By: caguerre <caguerre@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 12:14:43 by caguerre          #+#    #+#             */
-/*   Updated: 2022/05/09 12:17:27 by caguerre         ###   ########.fr       */
+/*   Updated: 2022/05/12 14:41:13 by caguerre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 int	ft_isalnum(int c)
 {
