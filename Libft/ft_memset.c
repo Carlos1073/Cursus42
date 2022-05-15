1d2192a1feead30de4158bbda612da864f98d30f
Carlos Guerrero garcia <caguerre@cbr7s4.42barcelona.com>
2022-05-15 10:50:39 +0200

    OK

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -6,11 +6,11 @@
 /*   By: caguerre <caguerre@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 14:57:37 by caguerre          #+#    #+#             */
-/*   Updated: 2022/05/10 14:05:24 by caguerre         ###   ########.fr       */
+/*   Updated: 2022/05/12 14:44:11 by caguerre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 void	*ft_memset(void *b, int c, size_t len)
 {
