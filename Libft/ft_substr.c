aa1ba3dd23217ed7ddfd6f0114be676e941fa62b
Carlos Guerrero garcia <caguerre@cbr11s6.42barcelona.com>
2022-05-16 17:25:09 +0200

    OK

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: caguerre <caguerre@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 14:36:54 by caguerre          #+#    #+#             */
-/*   Updated: 2022/05/12 14:47:11 by caguerre         ###   ########.fr       */
+/*   Updated: 2022/05/16 12:11:56 by caguerre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,22 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	int		i;
 	size_t	slen;
 	char	*new;
 
-	slen = ft_strlen((char *)(s));
-	i = 0;
 	if (!s)
 		return (NULL);
-	if (slen < start)
-		return (0);
-	new = malloc(sizeof(char) * (len - start + 1));
+	slen = ft_strlen(s);
+	if (len > (slen - start))
+		len = slen - start;
+	if (start > slen)
+		len = 0;
+	new = (char *)malloc(sizeof(char) * (len + 1));
 	if (!new)
 		return (NULL);
-	while (new[i])
-	new[i] = '\0';
+	if (len == 0)
+		new[0] = '\0';
+	else
+		ft_strlcpy(new, &((char *)s)[start], len + 1);
 	return (new);
 }
