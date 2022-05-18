4459ec7aaacec4fb124e71507c300682e74290d1
Carlos Guerrero garcia <caguerre@cbr11s4.42barcelona.com>
2022-05-18 17:19:47 +0200

    OK

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: caguerre <caguerre@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 16:30:33 by caguerre          #+#    #+#             */
-/*   Updated: 2022/05/18 16:30:53 by caguerre         ###   ########.fr       */
+/*   Updated: 2022/05/18 17:13:04 by caguerre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,65 +57,3 @@ char	**ft_split(char const *s, char c)
 	array[r] = NULL;
 	return (array);
 }
-/*
-
-static int	ft_count_words(char const *s, char c)
-{
-	int	i;
-	int	words;
-
-	words = 0;
-	i = 0;
-	while(s[i] != '\0')
-	{	
-		if (i > 0 && s[i] != c && s[i - 1] == c)
-			words++;
-		if (i == 0 && s[i] != c)
-			words++;
-		i++;
-	}
-	//words++;
-	return (words);
-}
-
-static char	ft_result(char const *s, char **array, char c)
-{
-	int		i;
-	int		j;
-	size_t	winit;
-	size_t	wend;
-
-	i = 0;
-	j = 0;
-	while (s[i] != '\0')
-	{
-		if (i > 0 && s[i] != c && s[i - 1] == c)
-			winit = i;
-		if (i == 0 && s[0] != c)
-			winit = 0;
-		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
-		{
-			wend = i;
-			array[j] = ft_substr(s, winit, (wend - winit + 1));
-			j++;
-		}
-		i++;
-	}
-	return (**array);
-}
-char	**ft_split(char const *s, char c)
-{
-	int		j;
-	int		r;
-	char	**array;
-	
-	j = 0;
-	r = ft_count_words(s, c);
-	array = (char **)malloc(sizeof (char *) * (r + 1));
-	if (!array)
-		return (NULL);
-	ft_result(s, array, c);
-	array[j] = NULL;
-	return (array);
-}
-*/
