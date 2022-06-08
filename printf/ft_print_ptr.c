ed8fd596ff18f7a5d2739f8794764bce43c83607
Carlos Guerrero garcia <caguerre@cbr6s4.42barcelona.com>
2022-06-08 14:25:41 +0200

    actualizar

--- printf/ft_print_ptr.c
+++ printf/ft_print_ptr.c
@@ -1,13 +1,31 @@
-#include "libr_printf.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_ptr.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caguerre <caguerre@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/07 15:30:24 by caguerre          #+#    #+#             */
+/*   Updated: 2022/06/07 15:35:42 by caguerre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	ft_print_ptr(void *str)
+#include "printf.h"
+
+int	ft_print_ptr(void *ptr)
 {
-	int	i;
+	unsigned long	nbr;
+	int	len;
 
-	i = 0;
-	while ()
+ 	len = 0;
+	if (!str = NULL)
 	{
-
+		nbr = (unsigned long long)ptr;
+		len += write(1, "0x", 2);
+		len += ft_print_hexa(nbr);
+		return (len);
 	}
-	return (i);
+	else
+		len =+ write(1, "0x0", 3);
+	return (len);
 }
