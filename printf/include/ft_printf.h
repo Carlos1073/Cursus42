c9bcd04b0b959af61f75266a1ee8a5811495685c
Carlos Guerrero garcia <caguerre@cbr14s3.42barcelona.com>
2022-07-06 13:04:12 +0200

    Todo OK con writes protegidos

---
+++ printf/include/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caguerre <caguerre@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/13 12:36:58 by caguerre          #+#    #+#             */
+/*   Updated: 2022/06/13 12:37:08 by caguerre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <limits.h>
+# include "../libft/libft.h"
+
+int	ft_printf(char const *str, ...);
+int	ft_print_char(int c);
+int	ft_print_string(char *s);
+int	ft_print_ptr(void *ptr);
+int	ft_print_hexa(unsigned long int nbr, char *base);
+int	ft_print_dec(int nbr);
+int	ft_print_unsigned(unsigned int nbr);
+
+#endif
