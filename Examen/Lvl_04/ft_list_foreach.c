bf52ec3e6c07c359fad29c9c15053cbcf6c31599
Carlos Guerrero garcia <caguerre@cbr9s4.42barcelona.com>
2022-09-28 16:13:06 +0200

    Practicas

---
+++ Examen/Lvl_04/ft_list_foreach.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caguerre <caguerre@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/28 14:50:22 by caguerre          #+#    #+#             */
+/*   Updated: 2022/09/28 14:57:58 by caguerre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void    ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+	t_list	*list_ptr;
+
+	list_ptr = begin_list;
+	while (list_ptr)
+	{
+		(*f)(list_ptr->data);
+		list_ptr = list_ptr->next;
+	}
+}
---
+++ Examen/Lvl_04/ft_list_remove_if.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_remove_if.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caguerre <caguerre@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/28 14:58:58 by caguerre          #+#    #+#             */
+/*   Updated: 2022/09/28 15:24:33 by caguerre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+{
+	t_list	*new;
+
+	new = begin_list;
+	if ((*cmp)(new->data, data_ref) == 0)
+	{
+		*begin_list = new->next;
+		free(new);
+		ft_list_remove_if(begin_list, data_ref, cmp);
+	}
+	new = *begin_list;
+	ft_list_remove_if(&new->nevtm data_ref, cmp);
+}
