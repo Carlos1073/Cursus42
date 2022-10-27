30a54fdf556a0de3d5c6e7d7d22dd56472b3a2df
Carlos Guerrero garcia <caguerre@cbr11s6.42barcelona.com>
2022-10-27 15:13:09 +0200

    OK

---
+++ Examen/Lvl_03/lcm.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lcm.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caguerre <caguerre@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/27 14:57:10 by caguerre          #+#    #+#             */
+/*   Updated: 2022/09/27 15:38:13 by caguerre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+unsigned int    lcm(unsigned int a, unsigned int b)
+{
+    unsigned int    n;
+
+    if (a == 0 || b == 0)
+        return (0);
+    if (a > b)
+        n = a;
+    else
+        n = b;
+    while (1)
+    {
+        if (n % a == 0 && n % b == 0)
+            return (n);
+        ++n;
+    }
+}
