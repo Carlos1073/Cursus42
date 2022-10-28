9408dd3245c66f13a81c8642e6d33a0417b34c24
Carlos Guerrero garcia <caguerre@cbr11s6.42barcelona.com>
2022-10-28 14:14:51 +0200

    OK

--- Examen/Lvl_02/max.c
+++ Examen/Lvl_02/max.c
@@ -13,27 +13,29 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int		max(int* tab, unsigned int len)
+int max(int *tab, unsigned int len)
 {
-	int	i;
-	int	n_max;
+	int result;
+	int i;
 	
-	i = 0;
-	n_max = tab[0];
+	result = tab[i];
+	i = 0;	
 	if (!len)
 		return (0);
-	while (tab[i] <= len)
+	while(i < len)
 	{
-		if (tab[i] > n_max)
-			n_max = tab[i];
+		if (result <  tab[i])
+		{
+			result = tab[i];
+		}
 		i++;
 	}
-	return (n_max);
+	return result;
 }
 
 int	main(void)
 {
-	int				data[5] = {19, 10, 8, 17, 9};
+	int				data[5] = {19, 20, 8, 30, 9};
 	unsigned int	len;
 	int				res;
 
