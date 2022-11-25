47e8a9165f44db1986c17942c1d77a6c9587993f
Carlos Guerrero garcia <caguerre@car13s6.42barcelona.com>
2022-11-25 13:37:01 +0100

    Incorporado mensaje error de path

--- so_long/sources/errors.c
+++ so_long/sources/errors.c
@@ -15,18 +15,20 @@
 void	ft_errors(t_game *game, int i)
 {
 	if (i == ERROR_0)
-		printf("Error: Invalid parameters!\n");
+		ft_printf("Error: Invalid parameters!\n");
 	else if (i == ERROR_1)
-		printf("Error: Invalid walls!\n");
+		ft_printf("Error: Invalid walls!\n");
 	else if (i == ERROR_2)
-		printf("Error: Problem with player, collectable or exit!\n");
+		ft_printf("Error: Problem with player, collectable or exit!\n");
 	else if (i == ERROR_3)
-		printf("Error!\n");
+		ft_printf("Error!\n");
 	else if (i == ERROR_4)
-		printf("Error: Can't open the map!\n");
+		ft_printf("Error: Can't open the map!\n");
 	else if (i == ERROR_5)
-		printf("Error: It's not a rectangle!\n");
+		ft_printf("Error: It's not a rectangle!\n");
+	else if (i == ERROR_6)
+		ft_printf("Error: Map with invalid path!\n");
 	else if (i == ERROR_7)
-		printf("Error: The map is empty!\n");
+		ft_printf("Error: The map is empty!\n");
 	exit_game(game);
 }
