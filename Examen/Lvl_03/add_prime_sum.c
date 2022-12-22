9732721051e3923bc1aa2f62ba0f1d0bdb95cde8
Carlos Guerrero garcia <caguerre@cbr12s2.42barcelona.com>
2022-12-22 16:15:58 +0100

    OK

--- Examen/Lvl_03/add_prime_sum.c
+++ Examen/Lvl_03/add_prime_sum.c
@@ -17,29 +17,21 @@ int	ft_atoi(char *str)
 {
 	int	i;
 	int	res;
-	int	negative;
 	
 	i = 0;
 	res = 0;
-	negative = 1;
-	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-		i++;
-	i = 0;
-	if (str[i] == '-')
-		negative = -1;
-	i = 0;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		res = (res * 10) + (str[i] - '0');
 		i++;
 	}
-	return (res * negative);
+	return (res);
 }
 
 void	ft_putnbr(int num)
 {
 	char	c;
-	if (num >= 10)
+	if (num > 9)
 		ft_putnbr(num / 10);
 	c = (num % 10) + 48;
 	write(1, &c, 1);
@@ -49,36 +41,45 @@ int	is_prime(int num)
 {
 	int	i;
 
-	i = 3;
-	if (num <= 1)
-		return (0);
-	if (num % 2 == 0 && num > 2)
-		return (0);
-	while (i < (num / 2))
+	i = 2;
+	while (i < num)
 	{
 		if (num % i == 0)
 			return (0);
-		i += 2;
+		i++;
 	}
-	return 1;
+	return (1);
 }
 
 int	main(int argc, char **argv)
 {
-	int	number;
-	int	sum;
-	
-	sum = 0;
-	number = ft_atoi(argv[1]);
 	if (argc == 2)
 	{
-		while (number > 0)
-			if (is_prime(number--))
-				sum += (number + 1);
+		int	i;
+		int	number;
+		int	sum;
+
+		i = 2;
+		number = ft_atoi(argv[1]);
+		sum = 0;
+		if (number <= 0)
+		{
+			write(1, "0\n", 2);
+			return (0);
+		}
+		while (i < number)
+		{
+			if (is_prime(i))
+			{
+				sum += i;
+			}
+			i++;
+		}
+		if (is_prime(number))
+			sum += number;
 		ft_putnbr(sum);
+		write(1, "\n", 1);	
 	}
-	if (argc != 2)
-		ft_putnbr(0);
-	write(1, "\n", 1);
-	return (0);
+	else
+		write(1, "0\n", 2);
 }
