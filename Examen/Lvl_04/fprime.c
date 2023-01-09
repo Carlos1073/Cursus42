c4bf0e68fa1a1df2b7eb10d26ba7dbb7f687f2d3
Carlos Guerrero garcia <caguerre@car15s5.42barcelona.com>
2023-01-09 16:39:17 +0100

    OK

--- Examen/Lvl_04/fprime.c
+++ Examen/Lvl_04/fprime.c
@@ -1,62 +1,72 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	number;
+
+	if (argc == 2)
+	{
+		i = 1;
+		number = atoi(argv[1]);
+		if (number == 1)
+			printf("1");
+		while (number >= ++i)
+		{
+			if (number % i == 0)
+			{
+				printf("%d", i);
+				if (number == i)
+					break ;
+				printf("*");
+				number /= i;
+				i = 1;
+			}
+		}
+	}
+	printf("\n");
+	return (0);
+}
 
 // int main(int argc, char **argv)
 // {
-//     int i;
-//     int nbr;
+// 	if (argc != 2)
+// 	{
+// 		write(1, "\n", 1);
+// 		return (0);
+// 	}
 
-//     nbr = atoi(argv[1]);
-//     i = 2;
-//     if (argc == 2)
-//     {
-//         if (nbr == 1)
-//             printf("1");
-//         while (i <= nbr)
-//         {
-//             if (nbr % i == 0)
-//             {
-//                 prntf("%d", i);
-//                 if (nbr == i)
-//                     break ;
-//                 printf("*");
-//                 nbr = nbr / i;
-//                 i = 2;
-//             }
-//             i++;
-//         }
+// 	int nbr = atoi(argv[1]);
+// 	int i = 2;
+// 	int flag = 0;
 
-//     }
-//     printf("\n");
+// 	if (nbr == 1)
+// 		{
+// 			write(1, "1\n", 2);
+// 			return (0);
+// 		}
+// 	while (i <= nbr)
+// 	{
+// 		if (i == nbr)
+// 		{
+// 			if (flag > 0)
+// 				printf("*");
+// 			printf("%d", i);
+// 			// if (flag == 0)
+// 			// 	printf(" es numero primo \n");
+// 			break;
+// 		}
+// 		if (nbr % i == 0)
+// 		{
+// 			if (flag > 0)
+// 				printf("*");	
+// 			printf("%d", i);
+// 			nbr /= i;
+// 			flag++;
+// 			continue;
+// 		}
+// 		i++;
+// 	}
+// 	return (0);
 // }
-
-int main(int argc, char **argv)
-{
-	int nbr = atoi(argv[1]);
-	int i = 2;
-	int flag = 0;
-
-	while (i <= nbr)
-	{
-		if (i == nbr)
-		{
-			if (flag > 0)
-				printf("*");
-			printf("%d", i);
-			// if (flag == 0)
-			// 	printf(" es numero primo \n");
-			break;
-		}
-		if (nbr % i == 0)
-		{
-			if (flag > 0)
-				printf("*");	
-			printf("%d", i);
-			nbr /= i;
-			flag++;
-			continue;
-		}
-		i++;
-	}
-	return (0);
-}
