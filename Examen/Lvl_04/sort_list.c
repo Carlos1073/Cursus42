e8262583c12ab37c22411871b6c0875c08b9c7d7
Carlos Guerrero garcia <caguerre@cbr8s6.42barcelona.com>
2023-01-10 15:04:41 +0100

    OK

--- Examen/Lvl_04/sort_list.c
+++ Examen/Lvl_04/sort_list.c
@@ -1,24 +1,64 @@
-#include "ft_list.h"
+#include "list.h"
+ #include <stdio.h>
 #include <stdlib.h>
+
+//int ascending(int a, int b)
+//{
+//	return (a <= b);
+//}
+
 
 t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
 {
-    t_list  *temp;
-    int     swap;
+	int tmp;
+	t_list *lst_tmp = lst;
+	int flag = 1;
 
-    temp = lst;
-    while (lst->next != NULL)
-    {
-        if ((*cmp)(lst->data, lst->next->data) == 0)
-        {
-            swap = lst->data;
-            lst->data = lst->next->data;
-            lst->next->data = swap;
-            lst = temp;
-        }
-        else
-            lst = lst->next;
-    }
-    lst = temp;
-    return (lst);
+	while(flag)
+	{
+		flag = 0;
+		while(lst_tmp && lst_tmp->next)
+		{
+			if(cmp(lst_tmp->data, lst_tmp->next->data) == 0)
+			{
+				flag = 1;
+				tmp = lst_tmp->data;
+				lst_tmp->data = lst_tmp->next->data;
+				lst_tmp->next->data = tmp;
+			}
+			lst_tmp = lst_tmp->next;	
+		}
+		lst_tmp = lst;
+	}
+	return(lst);
 }
+
+
+
+//int	main(void)
+//{
+//	t_list *a = calloc(1, sizeof(t_list));
+//	t_list *b = calloc(1, sizeof(t_list));
+//	t_list *c = calloc(1, sizeof(t_list));
+//	c->data = 45;
+//	b->data = 73;
+//	b->next = c;
+//	a->data = 108;
+//	a->next = b;
+//
+//printf("\t\tfunc %d\n",ascending(b->data, c->data));
+//
+//	t_list *cur = a;
+//	while(cur)
+//	{
+//		printf("%d\n", cur->data);
+//		cur= cur->next;
+//	}
+//
+//	t_list *res = sort_list(a, ascending);
+//	while(res)
+//	{
+//		printf("\t%d\n", res->data);
+//		res = res->next;
+//	}
+//}
