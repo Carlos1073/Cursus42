c2ed348bca08871acab5a31435b1b45927b5fa58
Carlos Guerrero garcia <caguerre@cbr8s6.42barcelona.com>
2023-01-10 16:26:00 +0100

    OK

--- Examen/Lvl_03/ft_rrange.c
+++ Examen/Lvl_03/ft_rrange.c
@@ -3,23 +3,42 @@
 
 int *ft_rrange(int start, int end)
 {
-    int *ptr;
-    int i;
-    int len;
+	int size = end - start+1;
+	int *ptr;
+	int i = 0;
+	if (size)
+	{
+		ptr = (int*) malloc(sizeof(int) * size);
+		while(start<=end)
+		{
+			ptr[i] =end;
+			i++;
+			end--;
+		}
+		return ptr;
+	}
+return (NULL);
+}
 
-    i = 0;
-    len = end - start;
-    if (start > end)
-        return (ft_rrange(end, start));
-    ptr = malloc(sizeof(int) * len);
-    if (ptr)
-    {
-        while (i < len)
-        {
-            ptr[i] = start;
-            start++;
-            i++;
-        }
-    }
-    return (ptr);
+int	main(void)
+{
+	int	i;
+	int	check;
+	int	*ptr;
+	int	start = 1;
+	int	end = 3;
+
+	i = -1;
+	check = 0;
+	ptr = ft_rrange(start, end);
+	while (ptr[++i] <= end)
+	{
+		if (check == 1 && ptr[i] == 0)
+			break;
+		if (check == 0 && ptr[i] == 0)
+			check++;
+
+		printf("%d\n", ptr[i]);
+	}
+	return (0);
 }
