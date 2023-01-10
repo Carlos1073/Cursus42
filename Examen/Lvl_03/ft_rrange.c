e2bae2c8504e82e60b164661a1efe0c4c75435bd
Carlos Guerrero garcia <caguerre@cbr8s6.42barcelona.com>
2023-01-10 17:19:33 +0100

    OK

--- Examen/Lvl_03/ft_rrange.c
+++ Examen/Lvl_03/ft_rrange.c
@@ -3,42 +3,45 @@
 
 int *ft_rrange(int start, int end)
 {
-	int size = end - start+1;
+	int len;
 	int *ptr;
-	int i = 0;
-	if (size)
+	int i;
+
+	i = 0;
+	len = end - start;
+	if (len)
 	{
-		ptr = (int*) malloc(sizeof(int) * size);
-		while(start<=end)
+		ptr = malloc(sizeof(int) * len + 1);
+		while(start <= end)
 		{
-			ptr[i] =end;
+			ptr[i] = end;
 			i++;
 			end--;
 		}
-		return ptr;
+		return (ptr);
 	}
 return (NULL);
 }
 
-int	main(void)
-{
-	int	i;
-	int	check;
-	int	*ptr;
-	int	start = 1;
-	int	end = 3;
+// int	main(void)
+// {
+// 	int	i;
+// 	int	check;
+// 	int	*ptr;
+// 	int	start = 1;
+// 	int	end = 3;
 
-	i = -1;
-	check = 0;
-	ptr = ft_rrange(start, end);
-	while (ptr[++i] <= end)
-	{
-		if (check == 1 && ptr[i] == 0)
-			break;
-		if (check == 0 && ptr[i] == 0)
-			check++;
+// 	i = -1;
+// 	check = 0;
+// 	ptr = ft_rrange(start, end);
+// 	while (ptr[++i] <= end)
+// 	{
+// 		if (check == 1 && ptr[i] == 0)
+// 			break;
+// 		if (check == 0 && ptr[i] == 0)
+// 			check++;
 
-		printf("%d\n", ptr[i]);
-	}
-	return (0);
-}
+// 		printf("%d\n", ptr[i]);
+// 	}
+// 	return (0);
+// }
