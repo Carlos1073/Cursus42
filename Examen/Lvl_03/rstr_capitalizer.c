a31ae7cbf45bfcf37d09c6137e9578a7aa28ef19
Carlos Guerrero garcia <caguerre@cbr9s1.42barcelona.com>
2023-01-11 16:41:46 +0100

    OK

--- Examen/Lvl_03/rstr_capitalizer.c
+++ Examen/Lvl_03/rstr_capitalizer.c
@@ -3,48 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   rstr_capitalizer.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: caguerre <caguerre@student.42barcel>       +#+  +:+       +#+        */
+/*   By: caguerre <caguerre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/27 16:35:58 by caguerre          #+#    #+#             */
-/*   Updated: 2022/09/28 10:40:43 by caguerre         ###   ########.fr       */
+/*   Created: 2022/09/28 12:27:46 by caguerre          #+#    #+#             */
+/*   Updated: 2023/01/11 16:41:18 by caguerre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	rstr_capitalizer(char *str)
+void    rstr_capitalizer(char *str)
 {
-	int	i;
+    int i = 0;
 
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] >= 'A' && str[i] <= 'Z')
-			str[i] += 32;
-		i++;
-	}
-	i = 0;	
-	while (str[i])
-	{
-		if (str[i + 1] == ' ' || str[i + 1] == '\t' || str[i + 1] == '\0')
-			str[i] -= 32;
-		i++;
-	}
-	i = 0;
-	while (str[i])
-	{
-		write(1, &str[i], 1);
-		i++;
-	}
+    while (str[i])
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+            str[i] += 32;
+        if ((str[i] >= 'a' && str[i] <= 'z') && (str[i + 1] == ' ' \
+                    || str[i + 1] == '\t' || str[i + 1] == '\0'))
+            str[i] -= 32;
+        write(1, &str[i++], 1);
+    }
 }
 
 int	main(int argc, char **argv)
 {
 	int	i;
 
-	i = 0;
 	if (argc < 2)
+	{
 		write(1, "\n", 1);
+		return (0);
+	}	
 	else
 	{
 		i = 1;
