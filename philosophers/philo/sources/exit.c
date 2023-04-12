70ae3ad2bd96378c949802a6fc1fa144d5f145ea
Carlos Guerrero garcia <caguerre@car11s3.42barcelona.com>
2023-04-12 17:31:33 +0200

    OK

--- philosophers/philo/sources/exit.c
+++ philosophers/philo/sources/exit.c
@@ -6,7 +6,7 @@
 /*   By: caguerre <caguerre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 16:10:45 by caguerre          #+#    #+#             */
-/*   Updated: 2023/03/29 15:19:29 by caguerre         ###   ########.fr       */
+/*   Updated: 2023/04/12 17:27:23 by caguerre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,6 @@ void    exit_table(t_table *table, t_philo *philos)
     while (++i < table->n_philos)
         pthread_mutex_destroy(&(table->forks[i]));
     pthread_mutex_destroy(&(table->printing));
+    free(table->philosophers);
+    free(table->forks);
 }
