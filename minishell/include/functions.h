7fa3d884201f3f519b56eff279504c08b5d0d7f5
Carlos Guerrero garcia <caguerre@car12s3.42barcelona.com>
2023-04-26 18:09:24 +0200

    in progress

--- minishell/include/functions.h
+++ minishell/include/functions.h
@@ -6,13 +6,13 @@
 /*   By: caguerre <caguerre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 15:54:08 by caguerre          #+#    #+#             */
-/*   Updated: 2023/04/26 16:49:27 by caguerre         ###   ########.fr       */
+/*   Updated: 2023/04/26 18:02:07 by caguerre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FUNCTIONS_H
 # define FUNCTIONS_H
 
-bool    print_message(bool return_val);
+char    print_message(char return_val);
 
 #endif
